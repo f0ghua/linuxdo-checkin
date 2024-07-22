9f4c19f7aa100410a05a769ecf6fc9c31a27f5f9
fog <fog.hua@gmail.com>
2024-07-22 09:59:42 +0800

    + add read_full_page

---
+++ read_full_page.py
@@ -0,0 +1,106 @@
+import os
+import time
+import random
+
+from tabulate import tabulate
+from playwright.sync_api import sync_playwright
+
+# https://linux.do/new
+# https://linux.do/search?q=after%3A2024-07-19
+
+USERNAME = "f0g" #os.environ.get("USERNAME")
+PASSWORD = "0x66o0x67.Do" #os.environ.get("PASSWORD")
+
+HOME_URL = "https://linux.do/"
+
+def page_down_until_end(page, max_page):
+    for i in range(max_page): #make the range as long as needed
+        page.keyboard.down("PageDown")
+        time.sleep(1)
+        print("Page down", i)
+
+class LinuxDoBrowser:
+    def __init__(self) -> None:
+        self.pw = sync_playwright().start()
+        proxy_settings = {
+            "server": "http://127.0.0.1:2802"
+        }
+        self.browser = self.pw.firefox.launch(headless=True)
+        self.context = self.browser.new_context(proxy=proxy_settings)
+        self.page = self.context.new_page()
+        self.page.goto(HOME_URL)
+
+    def login(self):
+        self.page.click(".login-button .d-button-label")
+        time.sleep(2)
+        self.page.fill("#login-account-name", USERNAME)
+        time.sleep(2)
+        self.page.fill("#login-account-password", PASSWORD)
+        time.sleep(2)
+        self.page.click("#login-button")
+        time.sleep(10)
+        user_ele = self.page.query_selector("#current-user")
+        if not user_ele:
+            print("Login failed")
+            return False
+        else:
+            print("Check in success")
+            return True
+
+    def click_topic(self):
+        for topic in self.page.query_selector_all("#list-area .title"):
+            page = self.context.new_page()
+            page.goto(HOME_URL + topic.get_attribute("href"))
+            time.sleep(3)
+#            if random.random() < 0.02:  # 100 * 0.02 * 30 = 60
+#                self.click_like(page)
+#            time.sleep(3)
+            #page_down_until_end(page)
+            page.close()
+
+    def click_long_topic(self, url, max_page=300):
+        page = self.context.new_page()
+        page.goto(url)
+        time.sleep(3)
+        page_down_until_end(page, max_page)
+        page.close()
+
+
+    def run(self):
+        if not self.login():
+            return
+        #self.click_topic()
+        self.click_long_topic("https://linux.do/t/topic/26876", 3300)
+        self.print_connect_info()
+
+    def click_like(self, page):
+        page.locator(".discourse-reactions-reaction-button").first.click()
+        print("Like success")
+
+    def print_connect_info(self):
+        page = self.context.new_page()
+        page.goto("https://connect.linux.do/")
+        rows = page.query_selector_all("table tr")
+
+        info = []
+
+        for row in rows:
+            cells = row.query_selector_all("td")
+            if len(cells) >= 3:
+                project = cells[0].text_content().strip()
+                current = cells[1].text_content().strip()
+                requirement = cells[2].text_content().strip()
+                info.append([project, current, requirement])
+
+        print("--------------Connect Info-----------------")
+        print(tabulate(info, headers=["项目", "当前", "要求"], tablefmt="pretty"))
+
+        page.close()
+
+
+if __name__ == "__main__":
+    if not USERNAME or not PASSWORD:
+        print("Please set USERNAME and PASSWORD")
+        exit(1)
+    l = LinuxDoBrowser()
+    l.run()
